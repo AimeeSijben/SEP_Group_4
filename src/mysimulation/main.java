0c32fab591ec1a2f982c0f730fed62c4c97089ea
lucasvankrevel <lucaskrevel1@gmail.com>
2025-06-09 22:01:58 +0200

    version 1

--- src/mysimulation/main.java
+++ src/mysimulation/main.java
@@ -1,5 +1,66 @@
 package mysimulation;
 
-public class main {
+import java.util.Scanner;
 
+public class main {
+    private static int clockTime = 2000;
+    private volatile boolean paused = false;
+    private volatile boolean running = true;
+
+    private void Commands(Scanner commands){
+        while(true){
+            String input = commands.nextLine().trim().toLowerCase();
+            switch (input) {
+                case "start":
+                    paused = false;
+                    System.out.println("continue.\n");
+                    break;
+                case "pause":
+                    paused = true;
+                    break;
+                case "exit":
+                    running = false;
+                    System.out.println("exit.\n");
+                    break;
+            }
+        }
+    }
+
+
+    public void Simulate(){
+
+        Scanner scanner = new Scanner(System.in);
+        Thread commandThread = new Thread(() -> Commands(scanner));
+        commandThread.setDaemon(true);
+        commandThread.start();
+
+        while (running) {
+            if (!paused){
+                /*
+                loop(trafic lights){
+                    if( color =green ){
+                        remove care from que at stop light
+                        add that care to moving
+                    } esle{
+                    do nothing
+                    }
+                } loop is for later if we want more intersections
+                from vihevile add new vihecile
+                */
+            }else{
+                System.out.println("Paused.\n");
+            }
+            try {
+                Thread.sleep(clockTime); // 1000 milliseconds = 1 second
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        main simulation = new main();
+        simulation.Simulate();
+    }
 }
