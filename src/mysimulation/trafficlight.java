4da8eca68fb6ddac0c680efc9c52aac467243881
brunovanvliet <brunovanvliet05@gmail.com>
2025-06-10 13:57:08 +0200

    trafficlight begin

--- src/mysimulation/trafficlight.java
+++ src/mysimulation/trafficlight.java
@@ -1,14 +1,44 @@
 package mysimulation;
 
-public class trafficlight {
+public class TrafficLight {
     enum State {
         RED, GREEN
     }
 
-    State current;
-    int timer;
+    State currentState;
+    int cycleLength;
+    Direction direction;
 
-    void update() {
-        // logic here
+    boolean iseven(int x) {
+        return (x & 1) == 0;
+    }
+
+    public TrafficLight(int cycleLength, Direction dir) {
+        if (cycleLength <= 0) {
+            throw new IllegalArgumentException("Cycle length must be positive.");
+        }
+        this.cycleLength = cycleLength;
+        this.currentState = State.RED; // Default initial state
+        direction = dir;
+    }
+
+    boolean isConflict(TrafficLight a, TrafficLight b) {
+        return false;
+    }
+
+    void setState(State newState) {
+        currentState = newState;// TODO: check situation
+    }
+
+    public State getState() {
+        return currentState;
+    }
+
+    void update(int timer) {
+        State newState = iseven(timer / cycleLength) ? State.GREEN : State.RED;
+        if (currentState != newState) {
+            setState(newState);
+        }
+
     }
 }
