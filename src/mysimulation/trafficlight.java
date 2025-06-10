f2b8f8fab8f401f2031b748907138b23ca588ae5
brunovanvliet <brunovanvliet05@gmail.com>
2025-06-10 15:21:44 +0200

    conflict

--- src/mysimulation/trafficlight.java
+++ src/mysimulation/trafficlight.java
@@ -13,6 +13,21 @@ public class TrafficLight {
         return (x & 1) == 0;
     }
 
+    int max(int a,int b) {  
+        return a>b?a:b;
+
+    int min(int a,int b) {  
+                return a>b?b:a;  
+    }
+    boolean isPerpendicular(Direction a, Direction b){
+
+        return (a == Direction.NORTH && (b == Direction.EAST || b==Direction.WEST)) ||
+           (a == Direction.SOUTH && (b == Direction.EAST || b==Direction.WEST))||
+           (b == Direction.NORTH && (a == Direction.EAST || a==Direction.WEST)) ||
+           (b == Direction.NORTH && (a == Direction.EAST || a==Direction.WEST)) ;
+           
+    }
+
     public TrafficLight(int cycleLength, Direction dir) {
         if (cycleLength <= 0) {
             throw new IllegalArgumentException("Cycle length must be positive.");
@@ -23,7 +38,10 @@ public class TrafficLight {
     }
 
     boolean isConflict(TrafficLight a, TrafficLight b) {
-        return false;
+        int maxcycle = max(a.cycleLength, b.cycleLength);
+        int mincycle = min(a.cycleLength, b.cycleLength);
+
+        return isPerpendicular(a.direction, b.direction) && (maxcycle % mincycle == 0);
     }
 
     void setState(State newState) {
