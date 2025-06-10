b6e902eb4c292ba6ed9ebdeecd6db731aa1eacb9
lucasvankrevel <156173620+lucasvankrevel@users.noreply.github.com>
2025-06-10 14:28:47 +0200

    Update cell.java

--- src/mysimulation/cell.java
+++ src/mysimulation/cell.java
@@ -1,4 +1,35 @@
 package mysimulation;
 
 public class cell {
+    public enum Type {
+        EMPTY,
+        ROADUP,
+        ROADDOWN,
+        ROADLEFT,
+        ROADRIGHT
+    }
+
+    private Type type;
+
+    public cell() {
+        this.type = Type.EMPTY;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public String toString() {
+        return switch (type) {
+            case ROADUP -> "↑";
+            case ROADDOWN -> "↓";
+            case ROADLEFT -> "←";
+            case ROADRIGHT -> "→";
+            default -> "E";
+        };
+    }
 }
