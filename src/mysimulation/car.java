d2cc1e59d8295695147e2e8b23a1f69a4a0816c5
AimeeSijben <aimeesijben@gmail.com>
2025-06-10 18:20:37 +0200

    Update car.java

--- src/mysimulation/car.java
+++ src/mysimulation/car.java
@@ -1,122 +1,127 @@
 package mysimulation;
 
-import java.awt.Color;
-import java.awt.Graphics;
+import mysimulation.Trafficlight.State;
 
-public class Car{
-    private Cell cell;
-    private Direction dir;
+public class Car {
+    private Cell currentCell;
+    private Direction currentDirection;
     private int speed;
-    private final int updatedSpeed;
-    private Color carColor;
-    private Trafficlight associatedTrafficlight;
+    private final int normalSpeed;
+    private Trafficlight associatedTrafficLight;
     private int stopDistance = 1;
 
-    // new car entering the grid
-    public Car(Cell cell, Direction dir, int updatedSpeed, Color carColor){
-        this.cell = initialCell;
-        this.dir = initialDir;
-        this.updatedSpeed = updatedSpeed;
-        this.speed = updatedSpeed;
-        this.carColor = carColor;
+    public Car(Cell initialCell, Direction initialDirection, int normalSpeed) {
+        this.currentCell = initialCell;
+        this.currentDirection = initialDirection;
+        this.normalSpeed = normalSpeed;
+        this.speed = normalSpeed;
     }
 
-    // check if the car should stop
+    public void setAssociatedTrafficLight(Trafficlight light, int stopDistance) {
+        this.associatedTrafficLight = light;
+        this.stopDistance = stopDistance;
+    }
+
     private boolean shouldStop(Cell potentialNextCell, Trafficlight light) {
-        if (light == null || !light.isRed()){
+        if (light == null || !light.isRed()) {
             return false;
         }
 
-        int LightStopX = light.getStopX();
-        int lightStopY = light.getStopY();
+        State lightStopX = light.getState();
+        State lightStopY = light.getState();
 
-        // calc a target stop coordinate
-        int targetStopX = lightStopX;
-        int targetStopY = lightStopY;
+        State targetStopX = lightStopX;
+        State targetStopY = lightStopY;
 
-    switch(dir){
-        case EAST: targetStopX -= stopDistance; break;
-        case WEST: targetStopX += stopDistance; break;
-        case NORTH: targetStopX += stopDistance; break;
-        case SOUTH: targetStopX -= stopDistance; break;
-    }
+        switch (currentDirection) {
+            case EAST: targetStopX -= stopDistance; break;
+            case WEST: targetStopX += stopDistance; break;
+            case NORTH: targetStopY += stopDistance; break;
+            case SOUTH: targetStopY -= stopDistance; break;
+        }
 
-    //check if ca's potential next cell would put the car at ot past the targeted cell
-    switch(dir){
-        case EAST: return potentialNextCell.getX() >= targetStopX;
-        case WEST: return potentialNextCell.getX() <= targetStopX;
-        case NORTH: return potentialNextCell.getY() <= targetStopY;
-        case SOUTH: return potentialNextCell.getY() >= targetStopY;
-        default: return false;
-    }
-}
-
-public void move(Grid grid){
-    Cell nextCell = getNextCell(grid);
-
-    boolean canMoveTick = true;
-    int currentSpeed = updatedSpeed;
-
-    // Check Traffic Light
-    if (associatedTrafficlight != null){
-        //ensure light is assigned
-        if (shouldStop(nextCell, associatedTrafficlight)){
-            //stop due to red light
-            currentSpeed = 0;
-            canMoveTick = false;
+        switch (currentDirection) {
+            case EAST: return potentialNextCell.getX() >= targetStopX;
+            case WEST: return potentialNextCell.getX() <= targetStopX;
+            case NORTH: return potentialNextCell.getY() <= targetStopY;
+            case SOUTH: return potentialNextCell.getY() >= targetStopY;
+            default: return false;
         }
     }
 
-    //check for other cars, if car should not be stopping
-    if (canMoveTick && currentSpeed > 0 && nextCell != null){
-        if (!grid.isCellEmpty(nextCell)){
-            //if cell is occupied by another car, or this car if speed > 1
-            if (grid.getCellOccupant(nextCell) != this){
-                currentSpeed = 0; //stop due to another car
-                canMoveTick = false;
+    public boolean update(Grid grid) {
+        Cell previousCell = this.currentCell;
+        Cell potentialNextCell = getNextCell(grid);
+
+        boolean canMoveThisTick = true;
+        int currentIntendedSpeed = normalSpeed;
+
+        if (associatedTrafficLight != null) {
+            if (shouldStop(potentialNextCell, associatedTrafficLight)) {
+                currentIntendedSpeed = 0;
+                canMoveThisTick = false;
             }
         }
+
+        if (canMoveThisTick && currentIntendedSpeed > 0 && potentialNextCell != null) {
+            if (!grid.isCellEmpty(potentialNextCell)) {
+                if (grid.getCellOccupant(potentialNextCell) != this) {
+                    currentIntendedSpeed = 0;
+                    canMoveThisTick = false;
+                }
+            }
+        }
+
+        this.speed = currentIntendedSpeed;
+
+        if (canMoveThisTick && this.speed > 0 && potentialNextCell != null) {
+            if (previousCell != null) {
+                grid.setCellOccupant(previousCell, null);
+            }
+            this.currentCell = potentialNextCell;
+            grid.setCellOccupant(this.currentCell, this);
+            return true;
+        } else if (potentialNextCell == null && this.speed > 0) {
+            if (previousCell != null) {
+                grid.setCellOccupant(previousCell, null);
+            }
+            this.currentCell = null;
+            return false;
+        } else {
+            return true;
+        }
     }
 
-    this.speed = currentSpeed;
+    public Cell getNextCell(Grid grid) {
+        if (currentCell == null) return null;
 
-    //moving the car if allowed
-    if (canMoveTick && this.speed > 0 && nextCell != null){
-        grid.setCellOccupant(cell, null); //empty old cell
-        this.cell = potentialNextCell; //update the car's position
-        grid.setCellOccuppant(cell, this); //occupy new cell with car
-    }
-    //if nextCell is null, the car will go off grid --> main loop removes the car
-}
+        int nextX = currentCell.getX();
+        int nextY = currentCell.getY();
 
-//determine next cell based on direction and speed
-public Cell getNextCell(Grid grid){
-    int x = cell.getX();
-    int y = cell.getY();
+        switch (currentDirection) {
+            case NORTH: nextY -= speed; break;
+            case SOUTH: nextY += speed; break;
+            case EAST: nextX += speed; break;
+            case WEST: nextX -= speed; break;
+        }
 
-    switch (dir){
-        case NORTH: nextY -= speed; break;
-        case SOUTH: nextY += speed; break;
-        case EAST: nextX -= speed; break;
-        case WEST: nextY += speed; break;
+        if (nextX >= 0 && nextX < grid.getCols() && nextY >= 0 && nextY < grid.getRows()) {
+            return grid.getCell(nextX, nextY);
+        }
+        return null;
     }
 
-    if (nextX >= 0 && nextX < grid.getCols() && nextY >= 0 && nextY < grid.getRows()) {
-        return grid.getCell(nextX, nextY);
-    }
-    return null; //car's off grid
-}
-
-    public void draw (Graphics g, int cellsize){
-        int xPix = cell.getX()*cellsize;
-        int yPix = cell.getY()*cellsize;
-
-        g.setColor(carColor);
-        g.fillRect(xPix + 2, yPix + 2, cellsize - 4, cellsize - 4); 
+    public char getConsoleChar() {
+        switch(currentDirection) {
+            case NORTH: return '^';
+            case SOUTH: return 'v';
+            case EAST:  return '>';
+            case WEST:  return '<';
+            default: return 'C'; // Default car representation
+        }
     }
 
-    public Cell getCurrentCell(){return cell;}
-    public Direction getCurrentDir(){return dir;}
-    public int getSpeed(){ return speed;}
-    public Color getCarColor(){ return carColor;}
+    public Cell getCurrentCell(){ return currentCell; }
+    public Direction getCurrentDirection(){ return currentDirection; }
+    public int getSpeed(){ return speed; }
 }
