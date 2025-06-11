4679a6072d6bb2e329468795cf50522c3e50450c
lucasvankrevel <156173620+lucasvankrevel@users.noreply.github.com>
2025-06-11 21:59:56 +0200

    Update trafficlight.java

--- src/mysimulation/trafficlight.java
+++ src/mysimulation/trafficlight.java
@@ -1,14 +1,64 @@
 package mysimulation;
 
-public class trafficlight {
+public class TrafficLight {
     enum State {
         RED, GREEN
     }
 
-    State current;
-    int timer;
+    State currentState;
+    int cycleLength;
+    Direction direction;
 
-    void update() {
-        // logic here
+    boolean iseven(int x) {
+        return (x & 1) == 0;
+    }
+
+    int max(int a,int b) {
+        return a>b?a:b;
+    }
+    int min(int a, int b) {
+        return a > b ? b : a;
+    }
+
+    boolean isPerpendicular(Direction a, Direction b) {
+
+        return (a == Direction.NORTH && (b == Direction.EAST || b == Direction.WEST)) ||
+                (a == Direction.SOUTH && (b == Direction.EAST || b == Direction.WEST)) ||
+                (b == Direction.NORTH && (a == Direction.EAST || a == Direction.WEST)) ||
+                (b == Direction.NORTH && (a == Direction.EAST || a == Direction.WEST));
+
+    }
+
+    public TrafficLight(int cycleLength, Direction dir) {
+        if (cycleLength <= 0) {
+            throw new IllegalArgumentException("Cycle length must be positive.");
+        }
+        this.cycleLength = cycleLength;
+        this.currentState = State.RED; // Default initial state
+        direction = dir;
+    }
+
+    boolean isConflict(TrafficLight a, TrafficLight b) {
+        int maxcycle = max(a.cycleLength, b.cycleLength);
+        int mincycle = min(a.cycleLength, b.cycleLength);
+
+        return isPerpendicular(a.direction, b.direction) && (maxcycle % mincycle == 0);
+    }
+
+    void setState(State newState) {
+        currentState = newState;// TODO: check situation
+    }
+
+    public State getState() {
+        return currentState;
+    }
+
+    void update(int timer) {
+        State newState = iseven(timer / cycleLength) ? State.GREEN : State.RED;
+        if (currentState != newState) {
+            setState(newState);
+        }
+
     }
 }
+
