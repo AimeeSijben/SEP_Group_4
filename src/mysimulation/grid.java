3c3ebadafbe2cd36bf302ae5bd9997c13724478f
lucasvankrevel <156173620+lucasvankrevel@users.noreply.github.com>
2025-06-11 16:26:35 +0200

    Update grid.java
    the movecare function is working but not really optimal

--- src/mysimulation/grid.java
+++ src/mysimulation/grid.java
@@ -38,28 +38,33 @@ public class grid {
                 }
             }
         }
-        placeTraficlight();
+        if(intersections != null) {
+            placeTraficlight();
+        }
     }
 
-    private void placeTraficlight(){
+    private void placeTraficlight() {
+        int row = intersections.row;
+        int col = intersections.col;
+
         // Check from UP
-        if (grid[intersections.row - 1][intersections.col].getType() == cell.Type.ROADDOWN) {
-            grid[intersections.row - 1][intersections.col].setTrafficLight(new TrafficLight(main.clockTime, Direction.NORTH));
+        if (row > 0 && grid[row - 1][col].getType() == cell.Type.ROADDOWN) {
+            grid[row - 1][col].setTrafficLight(new TrafficLight(main.clockTime, Direction.NORTH));
         }
 
         // Check from DOWN
-        if (grid[intersections.row + 1][intersections.col].getType() == cell.Type.ROADUP) {
-            grid[intersections.row + 1][intersections.col].setTrafficLight(new TrafficLight(main.clockTime, Direction.SOUTH));
+        if (row < rows - 1 && grid[row + 1][col].getType() == cell.Type.ROADUP) {
+            grid[row + 1][col].setTrafficLight(new TrafficLight(main.clockTime, Direction.SOUTH));
         }
 
         // Check from LEFT
-        if (grid[intersections.row][intersections.col - 1].getType() == cell.Type.ROADRIGHT) {
-            grid[intersections.row][intersections.col - 1].setTrafficLight(new TrafficLight(main.clockTime, Direction.WEST));
+        if (col > 0 && grid[row][col - 1].getType() == cell.Type.ROADRIGHT) {
+            grid[row][col - 1].setTrafficLight(new TrafficLight(main.clockTime, Direction.WEST));
         }
 
         // Check from RIGHT
-        if (grid[intersections.row][intersections.col + 1].getType() == cell.Type.ROADLEFT) {
-            grid[intersections.row][intersections.col + 1].setTrafficLight(new TrafficLight(main.clockTime, Direction.EAST));
+        if (col < collums - 1 && grid[row][col + 1].getType() == cell.Type.ROADLEFT) {
+            grid[row][col + 1].setTrafficLight(new TrafficLight(main.clockTime, Direction.EAST));
         }
     }
 
@@ -80,8 +85,72 @@ public class grid {
         Random random = new Random();
         int randomSpan = random.nextInt(spawns.size());
         Position spawn = spawns.get(randomSpan);
-        Car Car = new Car(main.clockTime);
+        Car Car = setCar(spawn);
         grid[spawn.row][spawn.col].setCar(Car);
+    }
+
+    private Car setCar(Position spawn) {
+        int row = spawn.row;
+        int col = spawn.col;
+
+        // Check from UP
+        if (row > 0 && grid[row - 1][col].getType() == cell.Type.ROADUP) {
+            return new Car(main.clockTime, Direction.NORTH);
+        }
+
+        // Check from DOWN
+        if (row < rows - 1 && grid[row + 1][col].getType() == cell.Type.ROADDOWN) {
+            return new Car(main.clockTime, Direction.SOUTH);
+        }
+
+        // Check from LEFT
+        if (col > 0 && grid[row][col - 1].getType() == cell.Type.ROADLEFT) {
+            return new Car(main.clockTime, Direction.WEST);
+        }
+
+        // Check from RIGHT
+        if (col < collums - 1 && grid[row][col + 1].getType() == cell.Type.ROADRIGHT) {
+            return new Car(main.clockTime, Direction.EAST);
+        }
+
+        // No incoming road found
+        return null;
+    }
+
+    public void moveCar() {
+        List<Position> moved = new ArrayList<>();
+
+        for (int r = 0; r < this.rows; r++) {
+            for (int c = 0; c < this.collums; c++) {
+                Car car = this.grid[r][c].getcar();
+
+                // Check manually if (r, c) is already in the moved list
+                boolean alreadyMoved = false;
+                for (Position p : moved) {
+                    if (p.row == r && p.col == c) {
+                        alreadyMoved = true;
+                        break;
+                    }
+                }
+
+                if (car != null && !alreadyMoved) {
+                    Position delta = car.getdir().move();
+                    int newRow = r + delta.row;
+                    int newCol = c + delta.col;
+
+                    // Check if new position is within bounds and valid
+                    if (newRow >= 0 && newRow < rows &&
+                            newCol >= 0 && newCol < collums &&
+                            this.grid[newRow][newCol].getcar() == null &&
+                            this.grid[newRow][newCol].getType() != cell.Type.EMPTY) {
+
+                        this.grid[newRow][newCol].setCar(car);
+                        moved.add(new Position(newRow, newCol));
+                    }
+                    this.grid[r][c].setCar(null);
+                }
+            }
+        }
     }
 
     public void printGrid() {
