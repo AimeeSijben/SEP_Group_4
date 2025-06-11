02722a9b0e4c66b6b0ac26e6d0aab01df585d5a3
lucasvankrevel <156173620+lucasvankrevel@users.noreply.github.com>
2025-06-11 21:57:53 +0200

    Update car.java

--- src/mysimulation/car.java
+++ src/mysimulation/car.java
@@ -1,10 +1,34 @@
 package mysimulation;
 
-public class car {
-    int x, y;
-    Direction dir;
+import java.util.ArrayList;
+import java.util.List;
 
-    void move() {
+public class Car{
+    private static long idCounter = 0;
+    private final long id;
+    private final long arrivalTick;
+    private final Direction dir;
 
+    public Car(long tick, Direction dir){
+        this.id = idCounter++;
+        this.arrivalTick = tick;
+        this.dir = dir;
+    }
+
+    public Direction getdir(){
+        return this.dir;
+    }
+
+    public long getId(){
+        return id;
+    }
+
+    public long getArrivalTick(){
+        return arrivalTick;
+    }
+
+    @Override
+    public String toString(){
+        return "Car-" + id + "arrived at: arrivalTick";
     }
 }
